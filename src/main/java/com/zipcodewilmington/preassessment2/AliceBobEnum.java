1628d40b8656ae66700df265ec3730190bc2f80e
Fenniless <fenniless@gmail.com>
2019-03-20 16:44:40 -0400

    some tests passing, still working

---
+++ src/main/java/com/zipcodewilmington/preassessment2/AliceBobEnum.java
@@ -0,0 +1,18 @@
+package com.zipcodewilmington.preassessment2;
+
+public enum AliceBobEnum {
+    ALICE,
+    BOB;
+
+    public String getCatchPhrase() {
+        return null;
+    }
+
+    public boolean isAlice() {
+        return false;
+    }
+
+    public boolean isBob() {
+        return false;
+    }
+}
---
+++ src/main/java/com/zipcodewilmington/preassessment2/AliceBobEvaluator.java
@@ -0,0 +1,17 @@
+package com.zipcodewilmington.preassessment2;
+
+public class AliceBobEvaluator {
+
+
+    public AliceBobEvaluator(String input) {
+
+    }
+
+    public boolean isAlice() {
+        return false;
+    }
+
+    public boolean isBob() {
+        return false;
+    }
+}
